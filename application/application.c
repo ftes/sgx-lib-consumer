d6905da81a0d1de8b0fd44016c4bea5aeec7a09f
Fredrik Teschke <f@ftes.de>
2016-04-26 13:10:24 +0200

    use new util functions to launch/destroy enclave

--- application/application.c
+++ application/application.c
@@ -1,26 +1,20 @@
 #include <stdio.h>
 #include <tchar.h>
+
 #include "sgx_urts.h"
 #include "sgx_lib.h"
 #include "enclave_u.h"
+#include "sgx_lib_u_util.h"
 
 #define ENCLAVE_FILE _T("enclave.signed.dll")
 
 int main(int argc, char* argv[])
 {
-  sgx_enclave_id_t   eid;
-  sgx_status_t       ret   = SGX_SUCCESS;
-  sgx_launch_token_t token = {0};
-  int updated = 0;
+  sgx_enclave_id_t eid;
   int secret;
 
   // Launch the enclave
-  // Token is not stored for now (would speed up subsequent launches)
-  ret  =  sgx_create_enclave(ENCLAVE_FILE,  SGX_DEBUG_FLAG,  &token,   &updated, &eid, NULL);
-  if (ret != SGX_SUCCESS) {
-    printf("App: error %#x, failed to create enclave.\n%s\n", ret, get_error_description(ret));
-    return -1;
-  }
+  if (launch_enclave(ENCLAVE_FILE, &eid) != SGX_SUCCESS) return -1;
 
   // Interact with the enclave
   printf("Secret to seal by the enclave (-1 to reuse old secret): ");
@@ -31,10 +25,7 @@ int main(int argc, char* argv[])
   print_secrets(eid);
 
   // Destroy the enclave
-  if(SGX_SUCCESS != sgx_destroy_enclave(eid)) {
-    printf("App: error %#x, failed to destroy enclave.\n%s\n", ret, get_error_description(ret));
-    return -1;
-  }
+  if(destroy_enclave(eid) != SGX_SUCCESS) return -1;
 
   printf("Press any key to exit...");
   getchar();
